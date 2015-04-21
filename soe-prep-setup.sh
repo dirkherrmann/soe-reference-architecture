f2f7ddbf92561a2041e91eee453b7eb0863c8f8f
Dirk Herrmann <dherrman@redhat.com>
2015-04-21 12:48:00 +0200

    removed functions

--- soe-prep-setup.sh
+++ soe-prep-setup.sh
@@ -4,7 +4,7 @@
 
 
 # check if exists and if yes source the config file 
-if  [[ -f '~/.soe-config' ]]
+if  $(test -f '~/.soe-config' )
 then
 	source '~/.soe-config'
 else
@@ -14,112 +14,82 @@ else
 fi
 
 
-# function calls
-pre_check()
-create_hammer_config()
-create_org()
-upload_manifest()
-rh_repo_sync()
-puppetforge_sync()
-ose3_sync()
+# check if organization already exists. if yes, exit
+hammer organization info --name $ORG >/dev/null 2>&1 || echo "Organization $ORG already exists. Exit."; exit 1
 
-def pre_check (
-	# check if organization already exists. if yes, exit
-	hammer organization info --name $ORG >/dev/null 2>&1 || echo "Organization $ORG already exists. Exit."; exit 1
+# this functions creates the yaml configuration file for hammer usage
+mkdir  ~/.hammer  
+cat << EOF > ~/.hammer/cli_config.yml  
+	:foreman:  
+	:host: $SATELLITE_SERVER
+	:username: $SATELLITE_USER
+	:password: $SATELLITE_PASSWD  
+	:organization: $ORG
+EOF 
 
-)
+# create org
+hammer organization create --name "$ORG" --label "$ORG" --description "SOE Reference Architecture example org"
 
+# upload manifest
+hammer subscription upload --organization "$ORG" --file "$subscription_manifest_loc"
+# note: has worked for me only at the second try, so maybe we should check if successful before proceeding:
+hammer subscription list --organization ACME | grep -q 'Red Hat Enterprise Linux Server' && echo ok || echo "Subscription import has not been successful. Exit"
 
-def create_hammer_config (
-	# this functions creates the yaml configuration file for hammer usage
-#mkdir  ~/.hammer  
-#cat << EOF > ~/.hammer/cli_config.yml  
-#	:foreman:  
-#	:host: $SATELLITE_SERVER
-#	:username: $SATELLITE_USER
-#	:password: $SATELLITE_PASSWD  
-#	:organization: $ORG
-#EOF 
+# RHEL7 repos
+hammer repository-set enable --organization $ORG --product 'Red Hat Enterprise Linux Server' --basearch='x86_64' --releasever='7Server' --name 'Red Hat Enterprise Linux 7 Server (Kickstart)'  
+hammer repository-set enable --organization $ORG --product 'Red Hat Enterprise Linux Server' --basearch='x86_64' --releasever='7Server' --name 'Red Hat Enterprise Linux 7 Server (RPMs)'  
+hammer repository-set enable --organization $ORG --product 'Red Hat Enterprise Linux Server' --basearch='x86_64' --releasever='7Server' --name 'Red Hat Enterprise Linux 7 Server - RH Common (RPMs)'  
+hammer repository-set enable --organization $ORG --product 'Red Hat Enterprise Linux Server' --basearch='x86_64' --releasever='7Server' --name 'RHN Tools for Red Hat Enterprise Linux 7 Server (RPMs)'  
 
-)
+# TODO do we need additional channels for Sat 6.1 (satellite-tools?)
 
-def create_org (
-	hammer organization create --name "$ORG" --label "$ORG" --description "SOE Reference Architecture example org"
+# RHEL6 repos only if RHEL6_ENABLED param is set to 1 in config file
+if $RHEL6_ENABLED == 1
+then
 
-)
-
-def upload_manifest (
-	hammer subscription upload --organization "$ORG" --file "$subscription_manifest_loc"
-	# note: has worked for me only at the second try, so maybe we should check if successful before proceeding:
-	hammer subscription list --organization ACME | grep -q 'Red Hat Enterprise Linux Server' && echo ok || echo "Subscription import has not been successful. Exit"
-
-)
-
-def rh_repo_sync (
-	# RHEL7 repos
-	hammer repository-set enable --organization $ORG --product 'Red Hat Enterprise Linux Server' --basearch='x86_64' --releasever='7Server' --name 'Red Hat Enterprise Linux 7 Server (Kickstart)'  
-	hammer repository-set enable --organization $ORG --product 'Red Hat Enterprise Linux Server' --basearch='x86_64' --releasever='7Server' --name 'Red Hat Enterprise Linux 7 Server (RPMs)'  
-	hammer repository-set enable --organization $ORG --product 'Red Hat Enterprise Linux Server' --basearch='x86_64' --releasever='7Server' --name 'Red Hat Enterprise Linux 7 Server - RH Common (RPMs)'  
-	hammer repository-set enable --organization $ORG --product 'Red Hat Enterprise Linux Server' --basearch='x86_64' --releasever='7Server' --name 'RHN Tools for Red Hat Enterprise Linux 7 Server (RPMs)'  
-
-	# TODO do we need additional channels for Sat 6.1 (satellite-tools?)
-
-
-	# RHEL6 repos only if RHEL6_ENABLED param is set to 1 in config file
-	if $RHEL6_ENABLED == 1
-	then
-		
-		hammer repository-set enable --organization $ORG --product 'Red Hat Enterprise Linux Server' --basearch='x86_64' --releasever='6Server' --name 'Red Hat Enterprise Linux 6 Server (Kickstart)'  
-		hammer repository-set enable --organization $ORG --product 'Red Hat Enterprise Linux Server' --basearch='x86_64' --releasever='6Server' --name 'Red Hat Enterprise Linux 6 Server (RPMs)'  
-		hammer repository-set enable --organization $ORG --product 'Red Hat Enterprise Linux Server' --basearch='x86_64' --releasever='6Server' --name 'Red Hat Enterprise Linux 6 Server - RH Common (RPMs)'  
-		hammer repository-set enable --organization $ORG --product 'Red Hat Enterprise Linux Server' --basearch='x86_64' --releasever='6Server' --name 'RHN Tools for Red Hat Enterprise Linux 6 Server (RPMs)'  
+	hammer repository-set enable --organization $ORG --product 'Red Hat Enterprise Linux Server' --basearch='x86_64' --releasever='6Server' --name 'Red Hat Enterprise Linux 6 Server (Kickstart)'  
+	hammer repository-set enable --organization $ORG --product 'Red Hat Enterprise Linux Server' --basearch='x86_64' --releasever='6Server' --name 'Red Hat Enterprise Linux 6 Server (RPMs)'  
+	hammer repository-set enable --organization $ORG --product 'Red Hat Enterprise Linux Server' --basearch='x86_64' --releasever='6Server' --name 'Red Hat Enterprise Linux 6 Server - RH Common (RPMs)'  
+	hammer repository-set enable --organization $ORG --product 'Red Hat Enterprise Linux Server' --basearch='x86_64' --releasever='6Server' --name 'RHN Tools for Red Hat Enterprise Linux 6 Server (RPMs)'  
   
-	fi
+fi
 
-	# sync all packages
-	hammer product synchronize --organization $ORG --name  'Red Hat Enterprise Linux Server' --async
+# sync all packages
+hammer product synchronize --organization $ORG --name  'Red Hat Enterprise Linux Server' --async
 
-	# set sync plan
-	hammer product set-sync-plan --sync-plan 'daily sync at 3 a.m.' --organization $ORG --name  'Red Hat Enterprise Linux Server' 
+# set sync plan
+hammer product set-sync-plan --sync-plan 'daily sync at 3 a.m.' --organization $ORG --name  'Red Hat Enterprise Linux Server' 
 
 
-	# TODO check if we sync already the repos for 3rd party software here (EPEL, Bareos, VMware Tools)
+# TODO check if we sync already the repos for 3rd party software here (EPEL, Bareos, VMware Tools)
 
-	# TODO additional repos 2 sync as defined in CONFIG section
-	echo "TODO"
+# TODO additional repos 2 sync as defined in CONFIG section
 
-)
 
-def epel_sync (
+# EPEL product including RHEL7 and RHEL6 (optional) repositories <- NOT POSSIBLE DUE TO 2 DIFFERENT GPG KEYS!
+hammer product create --name='EPEL7' --organization="$ORG"
+hammer repository create --name='EPEL7-x86_64' --organization="$ORG" --product='EPEL7' --content-type='yum' --publish-via-http=true --url=http://dl.fedoraproject.org/pub/epel/7/x86_64/
+hammer product set-sync-plan --sync-plan 'daily sync at 3 a.m.' --organization $ORG --name  "EPEL7"
 
-	# EPEL product including RHEL7 and RHEL6 (optional) repositories <- NOT POSSIBLE DUE TO 2 DIFFERENT GPG KEYS!
-	hammer product create --name='EPEL7' --organization="$ORG"
-	hammer repository create --name='EPEL7-x86_64' --organization="$ORG" --product='EPEL7' --content-type='yum' --publish-via-http=true --url=http://dl.fedoraproject.org/pub/epel/7/x86_64/
-	hammer product set-sync-plan --sync-plan 'daily sync at 3 a.m.' --organization $ORG --name  "EPEL7"
-
-	# EPEL Repo for RHEL6 repos only if EPEL6_ENABLED param is set to 1 in config file
-	if $EPEL6_ENABLED == 1
-	then
-		hammer product create --name='EPEL6' --organization="$ORG"
-		hammer repository create --name='EPEL6-x86_64' --organization="$ORG" --product='EPEL6' --content-type='yum' --publish-via-http=true --url=http://dl.fedoraproject.org/pub/epel/6/x86_64/
+# EPEL Repo for RHEL6 repos only if EPEL6_ENABLED param is set to 1 in config file
+if $EPEL6_ENABLED == 1
+then
+	hammer product create --name='EPEL6' --organization="$ORG"
+	hammer repository create --name='EPEL6-x86_64' --organization="$ORG" --product='EPEL6' --content-type='yum' --publish-via-http=true --url=http://dl.fedoraproject.org/pub/epel/6/x86_64/
 	hammer repository synchronize --organization "$ORG" --product "EPEL6" --async
-		# add it to daily sync plan
-		hammer product set-sync-plan --sync-plan 'daily sync at 3 a.m.' --organization $ORG --name  "EPEL6"
-	fi
-)
+	# add it to daily sync plan
+	hammer product set-sync-plan --sync-plan 'daily sync at 3 a.m.' --organization $ORG --name  "EPEL6"
+fi
 
-def ose3_sync (
-	# enable and sync OSE3 repositories
-	# TODO
-	echo "TODO" 
-)
-def puppetforge_sync (
-	if $PUPPETFORGE_ENABLED == 1
-	then
-		# TODO check if we really need the entire repo or just selected modules from it
-		# puppet forge repo
-		hammer product create --name='Forge' --organization=$ORG
-		hammer repository create --name='Puppet Forge' --organization=$ORG --product='Forge' --content-type='puppet' --publish-via-http=true --url=https://forge.puppetlabs.com
-	fi
-)
+# enable and sync OSE3 repositories
+# TODO
+echo "TODO" 
+
+if $PUPPETFORGE_ENABLED == 1
+then
+	# TODO check if we really need the entire repo or just selected modules from it
+	# puppet forge repo
+	hammer product create --name='Forge' --organization=$ORG
+	hammer repository create --name='Puppet Forge' --organization=$ORG --product='Forge' --content-type='puppet' --publish-via-http=true --url=https://forge.puppetlabs.com
+fi
 
