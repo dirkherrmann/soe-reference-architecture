a152142450169b2e96d05941246ffd0b2db35d34
Bryan Kearney <bryan.kearney@gmail.com>
2015-05-01 09:20:15 -0400

    Make ORG the name to be backwards compatible, and add quotes to get step1 to work

--- step1.sh
+++ step1.sh
@@ -29,10 +29,11 @@ EOF
 #hammer organization info --name $ORG >/dev/null 2>&1 && echo "Organization $ORG already exists. Exit."; exit 1
 
 # create org
-hammer organization create --name "$ORG_NAME" --label "$ORG" --description "$ORG_DESCRIPTION"
+hammer organization create --name "$ORG" --label "$ORG_LABEL" --description "$ORG_DESCRIPTION"
 
 # upload manifest
-hammer subscription upload --organization "$ORG_NAME" --file "$subscription_manifest_loc"
+hammer subscription upload --organization "$ORG" --file "$subscription_manifest_loc"
+
 # note: has worked for me only at the second try, so maybe we should check if successful before proceeding:
-hammer subscription list --organization $ORG | grep -q 'Red Hat Enterprise Linux Server' && echo ok || echo "Subscription import has not been successful. Exit"; exit
+hammer subscription list --organization "$ORG" | grep -q 'Red Hat Enterprise Linux Server' && echo ok || echo "Subscription import has not been successful. Exit"; exit
 
