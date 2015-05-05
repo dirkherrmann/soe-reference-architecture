1329fb89c54ddcb7c3c08997532a00b7dab8f70f
Dirk Herrmann <dherrman@redhat.com>
2015-05-05 13:39:58 +0200

    small bugfixes

--- common.sh
+++ common.sh
@@ -3,22 +3,22 @@
 # Common functions for all the step scripts
 
 # check if the config exists and if yes source the config file 
-if  [ ! -f ~/.soe-config ]; then
+if  [ ! -f $HOME/.soe-config ]; then
   echo "Could not find configuration file. Please copy the example file into your home directory and adapt it accordingly!"
   echo "# cp <path to your github copy>/soe-reference-architecture/soe-config.example ~/.soe-config"
   exit 1
 fi
 
-source ~/.soe-config
+source $HOME/.soe-config
 
 # Setup hammer based on the configurations
-mkdir  -p ~/.hammer  
-cat > ~/.hammer/cli_config.yml <<EOF 
+mkdir  -p $HOME/.hammer  
+cat > $HOME/.hammer/cli_config.yml <<EOF 
 :foreman:  
  :host: $SATELLITE_SERVER
  :username: $SATELLITE_USER
  :password: $SATELLITE_PASSWORD  
- :organization: $ORG_NAME
+ :organization: $ORG
 EOF
 
 echo "$ECHO_COMMANDS" 
--- soe-prep-setup.sh
+++ soe-prep-setup.sh
@@ -4,6 +4,10 @@
 
 # TODO make this script interactive to ask for each value and then automatically create the config file based on our template
 
+
+DIR="$PWD"
+source "${DIR}/common.sh"
+
 # check if exists and if yes source the config file 
 if  $(test -f '~/.soe-config' )
 then
@@ -17,6 +21,5 @@ fi
 
 # basically all long-duration tasks are now executed during step 1 and step 2
 DIR="$PWD"
-sh "${DIR}/step1.sh"
-sh "${DIR}/step2.sh"
+sh "${DIR}/step1.sh" && sh "${DIR}/step2.sh"
 
