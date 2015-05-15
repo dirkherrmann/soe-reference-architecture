bd7dbc618fefc49561479696e4a10a67c93a35a1
bkruell <bkruell@redhat.com>
2015-05-15 08:57:28 +0200

    Update step1.sh

--- step1.sh
+++ step1.sh
@@ -22,5 +22,5 @@ else
 fi
 
 # note: has worked for me only at the second try, so maybe we should check if successful before proceeding:
-hammer subscription list --organization "$ORG" | ( grep -q 'Rxed Hat' && echo ok ) || ( echo "Subscription import has not been successful. Exit"; exit 1 )
+hammer subscription list --organization "$ORG" | ( grep -q 'Red Hat' && echo ok ) || ( echo "Subscription import has not been successful. Exit"; exit 1 )
 
