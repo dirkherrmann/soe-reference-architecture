b6da3b0e110945fc52f7be24dcaa92a4ac8e601c
root <root@inf60.coe.muc.redhat.com>
2015-05-28 10:44:47 +0200

    fixed IFS for comma separated list

--- step2.sh
+++ step2.sh
@@ -1,10 +1,12 @@
 DIR="$PWD"
 source "${DIR}/common.sh"
 
+TMPIFS=$IFS
+IFS=","
 # create location
 for LOC in ${LOCATIONS}
 do
   hammer location create --name "${LOC}"
   hammer location add-organization --name "${LOC}" --organization "${ORG}"
 done
-
+IFS=$TMPIFS
