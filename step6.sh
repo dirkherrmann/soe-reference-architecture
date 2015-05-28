c5ccca883d74f59f77bb1dcd0ddebbfe41b4e0de
Dirk Herrmann <dherrman@redhat.com>
2015-05-28 09:57:06 +0200

    fixed wrong repo labels for extras and capsule and added check if main variable is empty

--- step6.sh
+++ step6.sh
@@ -125,7 +125,7 @@ hammer content-view  publish --name "cv-app-capsule" --organization "$ORG"  # --
 hammer content-view create --name "cv-app-docker" --description "Docker Host Content View" --organization "$ORG"
 # TODO add puppet repo and modules as well
 hammer content-view add-repository --organization "$ORG" --repository 'Red Hat Enterprise Linux 7 Server - Extras RPMs x86_64 7Server' --name "cv-app-docker" --product 'Red Hat Enterprise Linux Server'
-hammer content-view filter create --type rpm --name 'docker-package-only' --description 'Only include the docker rpm package' --inclusion=true --organization "$ORG" --repositories 'Red Hat Enterprise Linux 7 Server - Extras (RPMs)' --content-view "cv-app-docker"
+hammer content-view filter create --type rpm --name 'docker-package-only' --description 'Only include the docker rpm package' --inclusion=true --organization "$ORG" --repositories 'Red Hat Enterprise Linux 7 Server - Extras RPMs x86_64 7Server' --content-view "cv-app-docker"
 hammer content-view filter rule create --name docker --organization "$ORG" --content-view "cv-app-docker" --content-view-filter 'docker-package-only'
 # TODO let's try the latest version (no version filter). If we figure out that it does not work add a filter for docker rpm version here or inside the puppet module
 
