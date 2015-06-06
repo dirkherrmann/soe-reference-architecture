1836b7c7d3de21313fa9f0e64da2ae0d75d6312d
Dirk Herrmann <dherrman@redhat.com>
2015-06-06 11:50:45 +0200

    tested RHEL6 and added exclude filter example with emacs

--- step5.sh
+++ step5.sh
@@ -37,20 +37,28 @@ done
 
 ###################################################################################################
 #
-# RHEL 6 Core Build Content View - TODO NOT TESTED YET!
+# RHEL 6 Core Build Content View
 #
 ###################################################################################################
 if [ "$RHEL6_ENABLED" -eq 1 ]
 then
 	hammer content-view create --name "cv-os-rhel-6Server" --description "RHEL Server 6 Core Build Content View" --organization "$ORG"
 	# software repositories
-	hammer content-view add-repository --organization "$ORG" --name "cv-os-rhel-6Server" --repository 'Red Hat Enterprise Linux 6 Server Kickstart x86_64 6Server' --product 'Red Hat Enterprise Linux Server'
-	hammer content-view add-repository --organization "$ORG" --name "cv-os-rhel-6Server" --repository 'Red Hat Enterprise Linux 6 Server RPMs x86_64 6Server' --product 'Red Hat Enterprise Linux Server'
-	# TODO has to be substituted by 6.1 sat-tools channel which is not there yet
-	hammer content-view add-repository --organization "$ORG" --name "cv-os-rhel-6Server" --repository 'Red Hat Enterprise Linux 6 Server - RH Common RPMs x86_64 6Server' --product 'Red Hat Enterprise Linux Server'
+	hammer content-view add-repository --organization "$ORG" --name "cv-os-rhel-6Server" --repository 'Red Hat Enterprise Linux 6 Server Kickstart x86_64 6.5' --product 'Red Hat Enterprise Linux Server'
+	hammer content-view add-repository --organization "$ORG" --name "cv-os-rhel-6Server" --repository 'Red Hat Enterprise Linux 6 Server RPMs x86_64 6.5' --product 'Red Hat Enterprise Linux Server'
+	# TODO check if still required or covered by sat6-tools repo 
+	hammer content-view add-repository --organization "$ORG" --name "cv-os-rhel-6Server" --repository 'Red Hat Enterprise Linux 6 Server - RH Common RPMs x86_64 6.5' --product 'Red Hat Enterprise Linux Server'
+
+	# TODO  Red Hat Satellite Tools 6 Beta for RHEL 7 Server RPMs x86_64 7Server 
 
 	hammer content-view add-repository --organization "$ORG" --name "cv-os-rhel-6Server" --repository 'Zabbix-RHEL6-x86_64' --product 'Zabbix-Monitoring'
 	hammer content-view add-repository --organization "$ORG" --name "cv-os-rhel-6Server" --repository 'Bareos-RHEL6-x86_64' --product 'Bareos-Backup-RHEL6'
+
+	# exclude filter example using emacs package
+	# due to https://bugzilla.redhat.com/show_bug.cgi?id=1228890 you need to provide repository ID instead of name otherwise the filter applies to all repos 
+	REPOID=$(hammer --csv repository list --name 'Red Hat Enterprise Linux 6 Server RPMs x86_64 6.5' --organization $ORG | grep -vi '^ID' | awk -F',' '{print $1}')
+	hammer content-view filter create --type rpm --name 'excluding-emacs' --description 'Excluding emacs package' --inclusion=false --organization "$ORG" --repository-ids ${REPOID} --content-view "cv-os-rhel-6Server"
+	hammer content-view filter rule create --name 'emacs*' --organization "$ORG" --content-view "cv-os-rhel-6Server" --content-view-filter 'excluding-emacs'
 
 	# puppet modules which are part of core build 
 	# Note: since all modules are RHEL major release independent we're adding the same modules as for RHEL 7 Core Build
@@ -76,9 +84,14 @@ hammer content-view add-repository --organization "$ORG" --name "cv-os-rhel-7Ser
 hammer content-view add-repository --organization "$ORG" --name "cv-os-rhel-7Server" --repository 'Red Hat Enterprise Linux 7 Server RPMs x86_64 7Server' --product 'Red Hat Enterprise Linux Server'
 # TODO has to be substituted by 6.1 sat-tools channel which is not there yet
 hammer content-view add-repository --organization "$ORG" --name "cv-os-rhel-7Server" --repository 'Red Hat Enterprise Linux 7 Server - RH Common RPMs x86_64 7Server' --product 'Red Hat Enterprise Linux Server'
-# TODO check if still needed if we use Zabbix now. Changed from APP to CoreBuild instead.
 hammer content-view add-repository --organization "$ORG" --name "cv-os-rhel-7Server" --repository 'Zabbix-RHEL7-x86_64' --product 'Zabbix-Monitoring'
 hammer content-view add-repository --organization "$ORG" --name "cv-os-rhel-7Server" --repository 'Bareos-RHEL7-x86_64' --product 'Bareos-Backup-RHEL7'
+
+# exclude filter example using emacs package
+# due to https://bugzilla.redhat.com/show_bug.cgi?id=1228890 you need to provide repository ID instead of name otherwise the filter applies to all repos 
+REPOID=$(hammer --csv repository list --name 'Red Hat Enterprise Linux 7 Server RPMs x86_64 7Server' --organization $ORG | grep -vi '^ID' | awk -F',' '{print $1}')
+hammer content-view filter create --type rpm --name 'excluding-emacs' --description 'Excluding emacs package' --inclusion=false --organization "$ORG" --repository-ids ${REPOID} --content-view "cv-os-rhel-7Server"
+hammer content-view filter rule create --name 'emacs*' --organization "$ORG" --content-view "cv-os-rhel-7Server" --content-view-filter 'excluding-emacs'
 
 # we are creating an initial version just containing RHEL 7.0 bits based on a date filter between RHEL 7.0 GA and before RHEL 7.1 GA
 # TODO currently we can't update or delete the filter without UI since option list does not work. commenting the filter out until it works
