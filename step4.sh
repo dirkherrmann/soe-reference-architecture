527ec38e74463c0002c42033c943e90c958dccf3
Dirk Herrmann <dherrman@redhat.com>
2015-08-11 11:39:50 +0200

    fixed only 2 small typos

--- step4.sh
+++ step4.sh
@@ -17,13 +17,13 @@ hammer lifecycle-environment create --organization "$ORG" --name "DEV" --descrip
 hammer lifecycle-environment create --organization "$ORG" --name "QA" --description "Quality Assurance" --prior "DEV"
 hammer lifecycle-environment create --organization "$ORG" --name "PROD" --description "Production" --prior "QA"
 
-# create the dedicated lifecycle env path for amce-web
+# create the dedicated lifecycle env path for acme-web
 hammer lifecycle-environment create --organization "$ORG" --name "Web-DEV" --description "development" --prior "Library"
 hammer lifecycle-environment create --organization "$ORG" --name "Web-QA" --description "Quality Assurance" --prior "Web-DEV"
 hammer lifecycle-environment create --organization "$ORG" --name "Web-UAT" --description "Production" --prior "Web-QA"
 hammer lifecycle-environment create --organization "$ORG" --name "Web-PROD" --description "Production" --prior "Web-UAT"
 
-# create the dedicated lifecycle env path for amce-shop
+# create the dedicated lifecycle env path for acme-shop
 # TODO if we finally remove the shop we can remove this LC ENV path as well temporarily commented it out
 #hammer lifecycle-environment create --organization "$ORG" --name "Shop-DEV" --description "development" --prior "Library"
 #hammer lifecycle-environment create --organization "$ORG" --name "Shop-QA" --description "Quality Assurance" --prior "Shop-DEV"
